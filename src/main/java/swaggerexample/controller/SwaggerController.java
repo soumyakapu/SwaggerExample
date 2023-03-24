205d0ee32572aea5037c5b176af860edb3d14049
rajasekhar <mrrajasekhar09@gmail.com>
2023-03-24 18:00:17 +0530

    Demo on Swagger

---
+++ src/main/java/swaggerexample/controller/SwaggerController.java
@@ -0,0 +1,14 @@
+package swaggerexample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2")
+public class SwaggerController {
+    @GetMapping("/get")
+    public String getMessage(){
+        return "Welcome to swagger";
+    }
+}
